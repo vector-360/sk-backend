1893f70c4a480f9ed50232cf1226aab68d1c5932
Your Name <you@example.com>
2025-08-04 17:54:27 +0100

    Added folders/files. installed some dependencies....

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const path = require('path');
+
+
+//importing necessary modules
+const connectDB = require('./src/config/db.js');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+// Middleware
+app.use(express.json());
+app.use(morgan("dev"));
+
+
+//Routes
+app.get('/', (req, res) => res.send("Welcome to Softpire!"));
+
+//start the server
+app.listen(PORT, () => {
+    connectDB();
+  console.log(`Server is running on port ${PORT}`);
+});
